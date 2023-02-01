02a5a56d3cb3c6c743a29dcdf3cb068cb0c786f1
Russell Gordon <rgordon@lcs.on.ca>
2023-02-01 11:25:46 -0500

    Initial Commit

---
+++ MapOverlayExample/MapOverlayExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapOverlayExampleApp.swift
+//  MapOverlayExample
+//
+//  Created by Russell Gordon on 2023-02-01.
+//
+
+import SwiftUI
+
+@main
+struct MapOverlayExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
